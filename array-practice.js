5c2740c98cd1a97f1418db119630add0b2ff6835
thorhsu <thorshu@gmail.com>
2019-01-08 16:33:46 +0800

    Exercise01 finished

--- array-practice.js
+++ array-practice.js
@@ -1,21 +1,79 @@
 var people = ['Greg', 'Mary', 'Devon', 'James'];
-
 // 1. Using a loop, iterate through this array and console.log all of the people.
+console.log("1.---------------------");
+for(let person of people){
+    console.log(person);
+}
 
 // 2. Write the command to remove "Greg" from the array.
+console.log("2.---------------------");
+let slicePeople = people.slice(1);
+console.log(slicePeople);
 
 // 3. Write the command to remove "James" from the array.
+console.log("3.---------------------");
+let removePeople = (people, ...names) => {
+    for(let i = 0 ; i < people.length ; i++){
+        for(name of names){
+            if(people[i] === name){
+                people.splice(i , 1);
+                return people;
+            }
+        }
+    }        
+} 
+let peopleClone = Object.assign([], people);
+console.log(removePeople(peopleClone, "James"));
 
 // 4. Write the command to add "Matt" to the front of the array.
+console.log("4.---------------------");
+peopleClone = Object.assign([], people);
+peopleClone.unshift("Matt");
+console.log(peopleClone);
+
 
 // 5. Write the command to add your name to the end of the array.
+console.log("5.---------------------");
+peopleClone = Object.assign([], people);
+peopleClone.push("Thor");
+console.log(peopleClone);
+
 
 // 6. Using a loop, iterate through this array and after console.log-ing "Mary", exit from the loop.
+console.log("6.---------------------");
+for(let person of people){
+    console.log(person);
+    if(person === 'Mary'){
+        break;
+    }
+}
 
 // 7. Write the command to make a copy of the array using slice. The copy should NOT include "Mary" or "Matt".
+console.log("7.---------------------");
+peopleClone = [];
+let beginIdx = 0
+for(let i = 0 ; i < people.length ; i++){
+    let peopleClone2 = peopleClone;
+    if(people[i] === "Mary" || people[i] === "Matt"){
+        Object.assign(peopleClone, peopleClone2, people.slice(beginIdx, i));
+        beginIdx = i + 1; 
+    }else if(i == people.length - 1){
+        Object.assign(peopleClone, peopleClone2, people.slice(beginIdx, i + 1));
+    }
+}
+console.log(peopleClone);
+
 
 // 8. Write the command that gives the indexOf where "Mary" is located.
+console.log("8.---------------------");
+console.log(people.indexOf("Mary"));
 
 // 9. Write the command that gives the indexOf where "Foo" is located (this should return -1).
+console.log("9.---------------------");
+console.log(people.indexOf("Foo"));
+
 
 // 10. Redefine the people variable with the value you started with. Using the splice command, remove "Devon" from the array and add "Elizabeth" and "Artie". Your array should look like this when you are done ["Greg", "Mary", "Elizabeth", "Artie", "James"].
+console.log("10.---------------------");
+people.splice(2, 0, "Elizabeth", "Artie");
+console.log(people);
